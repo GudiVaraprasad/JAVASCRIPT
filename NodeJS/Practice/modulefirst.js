1447f6a61f76d604b34bd8e066a4b5855e980d35
Gudi Varaprasad <53944466+GudiVaraprasad@users.noreply.github.com>
2021-08-16 21:25:48 +0530

    Delete modulefirst.js

--- NodeJS/Practice/modulefirst.js
+++
@@ -1,6 +0,0 @@
-// import {simple, simple2} from "./modulesecond.mjs"
-import * as a2 from "./modulesecond.mjs"
-// const simple2 = require("./modulesecond.mjs");
-
-// simple23()
-console.log(a2.simple())
